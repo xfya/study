4e054cd40e2f0f3773645ae8b956a1c7f1e65317
xfya <349536842@qq.com>
2017-10-06 15:26:39 +0800

    1006

---
+++ 10/10-20/06/js/qbh.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+    sectionsColor: ['#fadd67', '#84a2d4', '#ef674d', '#ffeedd', '#d04759', '#84d9ed', '#4fdded', '#fff'],
+    navigation: true
+  });
+});
